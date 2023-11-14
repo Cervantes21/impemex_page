cfb054c0f9fafce16d901274c652a611205477f9
cervantes21 <admon.owlsoul@gmail.com>
2023-11-14 03:46:18 -0600

    template

--- main.py
+++ main.py
@@ -1,58 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-# Montar la carpeta "CSS" en la ruta "/CSS"
-app.mount("/CSS", StaticFiles(directory="CSS"), name="CSS")
-# Montar la carpeta "raw" en la ruta "/raw"
-app.mount("/raw", StaticFiles(directory="raw"), name="raw")
+# Configurar las plantillas de Jinja2
+templates = Jinja2Templates(directory=".")
 
-# Crear una ruta que devuelva una respuesta HTML
+# Montar la carpeta "assets" en la ruta "/assets"
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+
+# Crear una ruta que devuelva una respuesta HTML usando plantillas Jinja2
 @app.get("/", response_class=HTMLResponse)
-async def read_html():
-    # Lee el contenido del archivo HTML
-    with open("index.html", "r") as f:
-        html_content = f.read()
-    # Devuelve el contenido HTML como respuesta
-    return html_content
+async def read_index(request: Request):
+    # Puedes pasar datos a la plantilla si es necesario
+    data = {"request": request, "page_title": "Tu Título Aquí"}
+    # Renderizar la plantilla desde la carpeta raíz
+    return templates.TemplateResponse("index.html", {"request": request, "data": data})
 
+@app.get("/about", response_class=HTMLResponse)
+async def read_about(request: Request):
+    data = {"request": request, "page_title": "Acerca de Nosotros"}
+    return templates.TemplateResponse("about.html", {"request": request, "data": data})
 
-# from fastapi import FastAPI
-# from fastapi.staticfiles import StaticFiles
-# from fastapi.responses import HTMLResponse, StreamingResponse
-# import os
+@app.get("/shop", response_class=HTMLResponse)
+async def read_shop(request: Request):
+    data = {"request": request, "page_title": "Tienda"}
+    return templates.TemplateResponse("shop.html", {"request": request, "data": data})
 
-# app = FastAPI()
+@app.get("/contact", response_class=HTMLResponse)
+async def read_contact(request: Request):
+    data = {"request": request, "page_title": "Contacto"}
+    return templates.TemplateResponse("contact.html", {"request": request, "data": data})
 
-# # Montar la carpeta "CSS" en la ruta "/CSS"
-# app.mount("/CSS", StaticFiles(directory="CSS"), name="CSS")
-# # Montar la carpeta "raw" en la ruta "/raw"
-# app.mount("/raw", StaticFiles(directory="raw"), name="raw")
-
-# # Crear una ruta que devuelva una respuesta HTML con un video
-# @app.get("/", response_class=HTMLResponse)
-# async def read_html():
-#     # Lee el contenido del archivo HTML
-#     with open("index.html", "r") as f:
-#         html_content = f.read()
-#     # Devuelve el contenido HTML como respuesta
-#     return html_content
-
-# # Crear una ruta que transmita el archivo de video con el tiempo de inicio especificado
-# @app.get("/video")
-# async def video():
-#     def iterfile():
-#         with open("./raw/masterclass.mp4", mode="rb") as file_like:
-#             # Ajustar la posición del archivo para el tiempo de inicio especificado
-#             file_like.seek(402 * 1000)
-#             while True:
-#                 # Leer el archivo en trozos de 8KB
-#                 chunk = file_like.read(8192)
-#                 if not chunk:
-#                     break
-#                 yield chunk
-#     # Devolver la respuesta de transmisión (StreamingResponse)
-#     return StreamingResponse(iterfile(), media_type="video/mp4")
-
+@app.get("/shop-single", response_class=HTMLResponse)
+async def read_shop_single(request: Request):
+    data = {"request": request, "page_title": "Producto Individual"}
+    return templates.TemplateResponse("shop-single.html", {"request": request, "data": data})
